b72fe0b6d2796a41a2e6c402e055159f16d32f21
Doug Bradley <dpb070@g.harvard.edu>
2016-11-10 19:51:53 -0500

    additional effect, validation fixes

--- assignment3.js
+++ assignment3.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
+  // Doug Bradley
+  // DGMDE20 - Assignment 3
 
+
+  // phone field enable/disable from alerts checkbox
+  $('#phone').prop( 'disabled', true );
+  $('#alerts').click(function(){
+    if ($('#alerts').prop('checked')) {
+      $('#phone').prop( 'disabled', false );
+    }
+    else {
+      $('#phone').prop( 'disabled', true );
+    };
+  });
+
+
+
+  // Submit processing
   $('button').click(function(submit){
+    // Clear any error messages from previous failed submits
+    $('#name_error').html('');
+    $('#email_error').html('');
 
-$('#name_error').html('');
-$('#email_error').html('');
     // simple check for full name - 4 or more characters and includes space
     var nameLengthOK =  ($('#name').val().length > 3);
     var spaceFoundOK = ($('#name').val().indexOf(" ") > 0);
